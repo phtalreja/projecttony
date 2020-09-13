439e557173e713b0310ebc7185d5d92dc9bc1632
phtalreja <51252603+phtalreja@users.noreply.github.com>
2020-09-12 17:04:30 -0700

    reinit firebase app

---
+++ functions/index.js
@@ -0,0 +1,23 @@
+const functions = require('firebase-functions');
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyANc5OhpQ6vFTFs9VRTrNuFzMijhwmrZ_w",
+    authDomain: "pennapps2020-1df2f.firebaseapp.com",
+    databaseURL: "https://pennapps2020-1df2f.firebaseio.com",
+    projectId: "pennapps2020-1df2f",
+    storageBucket: "pennapps2020-1df2f.appspot.com",
+    messagingSenderId: "976023894018",
+    appId: "1:976023894018:web:b31fb21eb79ceb97f20f1b"
+  };
+  firebase.initializeApp(firebaseConfig);
+  // Initialize Cloud Functions through Firebase
+function onsubmit(){ 
+    var display = document.getElementById("result").innerText;
+    var query = document.getElementById("query").value;
+    var geocode = document.getElementById("geocode").value;
+    var opinionAnalysis = firebase.functions().httpsCallable('opinionAnalysis');
+    opinionAnalysis({query:query, geocode:geocode}).then(function(result) {
+    console.log(result);
+})
+    .catch(error => {  });
